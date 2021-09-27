a7bab4373688852aac0c2651c8c7d01d3d1c3d18
Guilherme Ramos <guilhermesramos@hotmail.com>
2021-09-27 14:51:48 -0300

    Apagando Controlador e Department view

--- CRUD - SalesWebMVC/Models/Department.cs
+++ CRUD - SalesWebMVC/Models/Department.cs
@@ -1,8 +1,11 @@
-﻿namespace CRUD___SalesWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD___SalesWebMVC.Models
 {
     public class Department
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
     }
 }
